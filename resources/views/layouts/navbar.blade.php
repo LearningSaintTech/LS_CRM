c83f47ce303ceb9db0e7694fd1d2788112057959
amarjeet-spec <amarjeet@learningsaint.com>
2025-10-15 20:50:12 +0530

    latest code

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="nav-header" style="background-color: #f39513; display: flex; align-items: center; justify-content: space-between; padding: 0.5rem 1rem;">
+<div class="nav-header" style="background-color: #ffc25ef2; display: flex; align-items: center; justify-content: space-between; padding: 0.5rem 1rem;">
     <a href="index.html" class="brand-logo" aria-label="Brand Logo" style="display: flex; align-items: center; text-decoration: none;">
         <!-- Logo Image -->
         <img src="{{ url('assets/images/learning_logo.svg') }}" alt="Learning Logo" style="height: 40px; width: auto;">
